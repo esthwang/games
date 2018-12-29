916e8100ca07f0f4e960404aea0d439a51ba9c9a
euirim <euirim@me.com>
2018-12-29 15:31:28 -0500

    Delete branch before pushing in deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,5 @@ touch out/CNAME
 echo "crossword.chicagomaroon.com" >> out/CNAME
 git add out/ 
 git commit -m "Deploy to gh-pages" 
+git push origin --delete gh-pages
 git subtree push --prefix out origin gh-pages

e42bc302aff6778b9ba417027b8114dd224a6528
euirim <euirim@me.com>
2018-12-29 17:46:23 -0500

    Deploy to gh-pages

---
+++ _next/static/chunks/1.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[1],{
+
+/***/ "./data.json":
+/*!*******************!*\
+  !*** ./data.json ***!
+  \*******************/
+/*! exports provided: 0, default */
+/***/ (function(module) {
+
+module.exports = ["2018-12-30 Test 1.json"];
+
+/***/ })
+
+}]);
+//# sourceMappingURL=1.js.map

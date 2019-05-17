3862fb54e9f566967784bffb86d3c76f463b3fd5
Chris Jones <christophsjones@gmail.com>
2019-05-17 11:20:29 -0500

    Deploy to gh-pages

---
+++ _next/static/chunks/2.051326222e9d0184be1e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{257:function(o){o.exports=["2019-02-01 Blizzard.json","2019-02-06 How Do You Spell That.json","2019-02-13 T Time.json","2019-03-06 Polar Core Texts.json","2019-03-13 Space is the Place.json","2019-04-03 Words to Amuse.json","2019-04-17 A Puzzle of Ice and Fire.json","2019-05-08 The Maroon Wants to Battle.json"]}}]);
